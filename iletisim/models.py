372c566045aec2c712d17f3b8413a6aec4813985
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2020-03-29 15:43:46 +0300

    Initial commit

---
+++ iletisim/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.utils import timezone
+
+class IletisimModel(models.Model):
+    adi = models.CharField(max_length=100)
+    soyadi = models.CharField(max_length=100)
+    email = models.EmailField()
+    yazi = models.TextField()
+    # telefon = models.PhoneNumberField()
+    zaman = models.DateTimeField(default=timezone.now)
+    CH_DEGERLENDIRME = [
+        ("5","Çok İyi"),
+        ("4","İyi"),
+        ("3","Kararsızım"),
+        ("2","Kötü"),
+        ("1","Çok Kötü")
+        ]
+    degerlendirme = models.CharField(choices=CH_DEGERLENDIRME,max_length=10)
+
+
+    def __str__(self):
+        return f"{self.id}-{self.adi} {self.soyadi}-{self.email}"
