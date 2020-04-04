1daacc30c50a7bab2b3c6822bdfc44b32a750223
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2020-04-04 17:10:25 +0300

    04042020_3

--- iletisim/urls.py
+++ iletisim/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('',views.iletiListe,name="iletiListe"), #127.0.0.1:8000/iletisim/
     path('detay/<int:pk>/',views.iletiDetay,name="iletiDetay"),
     path('ileti/yeni/',views.yeni_ileti,name="yeni_ileti"),
+    path('iletiduzenle/<int:pk>',views.iletiduzenle,name="iletiduzenle"),
 ]
--- iletisim/views.py
+++ iletisim/views.py
@@ -25,3 +25,15 @@ def yeni_ileti(request):
         form = IletisimForm()
     return render(request,"iletisim/duzenle.html",{"form":form})
 
+def iletiduzenle(request,pk):
+    ileti = get_object_or_404(IletisimModel,pk=pk)
+    if request.method == "POST":
+        form = IletisimForm(request.POST,instance=ileti)
+        if form.is_valid():
+            ileti = form.save(commit=False)
+            #######################
+            ileti.save()
+            return redirect('iletiDetay',pk=ileti.pk)
+    else:
+        form = IletisimForm(instance=ileti)
+    return render(request,"iletisim/duzenle.html",{"form":form})
