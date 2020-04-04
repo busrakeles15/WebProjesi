d9d886fc83424b567a0c743f369837e417b2f431
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2020-04-04 16:15:05 +0300

    04042020

--- iletisim/urls.py
+++ iletisim/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.iletiListe,name="iletiListe"), #127.0.0.1:8000/iletisim/
+    path('detay/<int:pk>/',views.iletiDetay,name="iletiDetay"),
 ]
--- iletisim/views.py
+++ iletisim/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import  IletisimModel
 
 def iletiListe(request):
     iletiler = IletisimModel.objects.all()
     return render(request,"iletisim/liste.html",
     {"iletis":iletiler})
+
+def iletiDetay(request,pk):
+    ileti = get_object_or_404(IletisimModel,pk=pk)
+    return render(request,"iletisim/detay.html",
+    {"ileti":ileti})
